7230cd91300ec5ea4d9a583529578f3b4fde7e62
marcofoppa <marcofoppa@yahoo.it>
2013-05-08 17:57:05 +0200

    Mercoledì

--- assets/www/js/stf/main.js
+++ assets/www/js/stf/main.js
@@ -1,6 +1,3 @@
-
-// Wait for Cordova to load
-//
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function Coordinate(latitudine, longitudine) {
@@ -22,11 +19,7 @@ function Piadineria(code, city, address, latitude, longitude, province, ragioneS
 }
 
 var GlobalPiadinerie = new Array();
-var _PiadineriaRow = '<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c"><a id="[code]" href="#dettaglioPiadineria" class="ui-link-inherit BtnPiadineria"><p class="ui-li-aside ui-li-desc"><strong>[distance]</strong> km <br />Percorrenza: <strong>[duration]</strong> min</p><h3 class="ui-li-heading">[ragioneSociale]</h3><p class="ui-li-desc"><strong>[address], [city]</strong></p></a></li>';
-
-
-// Cordova is ready
-//
+var _PiadineriaRow = '<li data-corners="false" data-shadow="false" data-iconshadow="true" data-wrapperels="div" data-icon="arrow-r" data-iconpos="right" data-theme="c" class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-btn-up-c"><a data-idx="[rowIndex]"  href="#dettaglioPiadineria" class="ui-link-inherit BtnPiadineria"><p class="ui-li-aside ui-li-desc"><strong>[distance]</strong> km <br />Percorrenza: <strong>[duration]</strong> min</p><h3 class="ui-li-heading">[ragioneSociale]</h3><p class="ui-li-desc"><strong>[address], [city]</strong></p></a></li>';
 
 function onError(error) {
     alert('code: ' + error.code + '\n' +
@@ -40,7 +33,7 @@ function LoadPiadinerie() {
     for (var i = 0; i <= GlobalPiadinerie.length - 1; i++) {
         var piad = GlobalPiadinerie[i];
         content += _PiadineriaRow
-            .replace("[code]", piad.code)
+            .replace("[rowIndex]", i)
             .replace("[distance]", piad.distance)
             .replace("[duration]", piad.duration)
             .replace("[ragioneSociale]", piad.ragioneSociale)
@@ -48,32 +41,29 @@ function LoadPiadinerie() {
             .replace("[address]", piad.address);
     }
 
-    $('#listapiadinerie').html(content);
-    $.mobile.changePage("#piadinerie", { transition: "slideup", changeHash: false });
+    //$('#listapiadinerie').html(content);
+
+
+    $(".BtnPiadineria").unbind("click").click(function ()
+    {
+        var codePiadineria = $(this).data('idx');
+
+        SetPiadineria(GlobalPiadinerie[codePiadineria]);
+
+    });
+    
+    $('#listapiadinerie').html(content).listview('refresh');
+    $.mobile.changePage("#piadinerie", { transition: "slideup", changeHash: true });
+ 
+    
 }
 
 
-function SetPiadineria(code) {
-
-    var piadineria = null;
-
-    for (var i = 0; i < GlobalPiadinerie.length; i++) {
-        if (GlobalPiadinerie[i].code === code) {
-            piadineria = GlobalPiadinerie[i];
-            break;
-        }
-    }
-
-    if (piadineria === null) {
-        alert('Piadineria non trovata');
-        $.mobile.changePage("#localizzazione", { transition: "slideup", changeHash: false });
-    }
-
-    
-
-
+function SetPiadineria(piadineria)
+{
     $.mobile.changePage("#dettaglioPiadineria", { transition: "slideup", changeHash: false });
-
+    
+    $("#map_canvas").html('').prepend(getMappaImg(piadineria.latitude, piadineria.longitude));
 }
 
 
@@ -88,32 +78,62 @@ function Init() {
         html: ""
     });
 
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-
-            GetPiadinerie(position.coords.latitude, position.coords.longitude, function () { LoadPiadinerie(); });
-           
-        },
-        onError);
+    GetCurrentPosition();
 
 }
 
-function onDeviceReady() {
+function GetCurrentPosition() {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            GetPiadinerie(position.coords.latitude, position.coords.longitude, function () { LoadPiadinerie(); });
+        },
+        onError);
+}
 
+function onDeviceReady() {
+    
     $(".BtnEsci").click(function () {
         navigator.app.exitApp();
     });
 
-    $(".BtnInit").click(function () {
+    $(".BtnInit").click(function (e) {
+        e.preventDefault();
         Init();
     });
+    
 
-    $(".BtnPiadineria").click(function() {
-        var codePiadineria = $(this).attr('code');
-
-        SetPiadineria(codePiadineria);
-
+    $(".BtnListaPiadinerie").click(function() {
+        GetCurrentPosition();
     });
 
     Init();
+}
+
+function getMappaImg(lat, lon)
+{
+    //Leggo le dimensioni dello schermo
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    //Imposto una dimensione quadrata utilizzando la dimensione minore (larghezza - altezza)
+    if (width > height)
+    {
+        width = height;
+    }
+    else if (height > width)
+    {
+        height = width;
+    }
+    
+    var zoom = 16;
+    
+    var content = "<img alt='' src='https://maps.googleapis.com/maps/api/staticmap?center=[lat],[lon]&zoom=[zoom]&size=[width]x[height]&markers=[lat],[lon]&sensor=false' width='[width]' height='[height]' />";
+
+    content = content.replace('[zoom]', zoom);
+    content = content.replace('[lat]', lat).replace('[lon]', lon);
+    content = content.replace('[lat]', lat).replace('[lon]', lon);
+    content = content.replace('[width]', width).replace('[height]', height);
+    content = content.replace('[width]', width).replace('[height]', height);
+
+    return content;
 }
--- assets/www/js/stf/mockup.js
+++ assets/www/js/stf/mockup.js
@@ -2,7 +2,7 @@
     var arrayPiadinerie = [];
 
     for (var i = 0; i < 10; i++) {
-        var piadineria = new Piadineria(i, "Citt&agrave; " + i, "Indirizzo " + i, "Lat " + i, "Long " + i, "Provincia " + i, "Piadineria n. " + i, "Regione " + i, i * 10, i * 5);
+        var piadineria = new Piadineria(i, "Citt&agrave; " + i, "Indirizzo " + i, "45.25285", "10.28785", "Provincia " + i, "Piadineria n. " + i, "Regione " + i, i * 10, i * 5);
         arrayPiadinerie.push(piadineria);
     }
     GlobalPiadinerie = arrayPiadinerie;
